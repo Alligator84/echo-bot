03332ac7e92256da194d2f0d15b7d1f99fffff6d
Oleg Yudin <olekat84@gmail.com>
2021-04-03 20:57:14 +0600

    Add echo-bot

---
+++ src/main/java/EchoBot.java
@@ -0,0 +1,32 @@
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Message;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class EchoBot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return "YurrencyFundsBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "1743209681:AAH96J8lHy0qy5UyIm0ACBnGYC8aAAUR4ko";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            Message incomingMessage = update.getMessage();
+            SendMessage outgoingMessage = new SendMessage();
+            outgoingMessage.setChatId(incomingMessage.getChatId().toString());
+            outgoingMessage.setText(incomingMessage.getText());
+            try {
+                execute(outgoingMessage);
+            } catch (TelegramApiException ex) {
+                System.err.println("Error echo-bot");
+            }
+        }
+    }
+}
